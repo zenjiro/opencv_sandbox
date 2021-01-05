a1e7f9ea61feab24548036720c368d4864d4fc7b
zenjiro <zenjiro0123@gmail.com>
2021-01-05 23:32:18 +0900

    複数ファイルを処理

--- detect_face_sample.py
+++ detect_face_sample.py
@@ -1,4 +1,8 @@
 # https://symfoware.blog.fc2.com/blog-entry-2413.html
+import glob
+import os
+import sys
+
 import cv2
 import numpy
 
@@ -6,23 +10,26 @@ prototxt = "deploy.prototxt"
 model = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
 confidence_limit = 0.5
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
-image = cv2.imread("face.jpg")
-h, w = image.shape[:2]
-blob = cv2.dnn.blobFromImage(
-    cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0)
-)
-net.setInput(blob)
-detections = net.forward()
-for i in range(0, detections.shape[2]):
-    confidence = detections[0, 0, i, 2]
-    if confidence < confidence_limit:
-        continue
-    box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-    start_x, start_y, end_x, end_y = box.astype("int")
-    text = "{:.2f}%".format(confidence * 100)
-    y = start_y - 10 if start_y - 10 > 10 else start_y + 10
-    cv2.rectangle(image, (start_x, start_y), (end_x, end_y), (0, 0, 255), 2)
-    cv2.putText(
-        image, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2
+output_directory = "output"
+os.makedirs(output_directory, exist_ok=True)
+for file in sum([glob.glob(x) for x in sys.argv[1:]], []):
+    image = cv2.imread(file)
+    h, w = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0)
     )
-cv2.imwrite("result.jpg", image)
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence < confidence_limit:
+            continue
+        box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
+        start_x, start_y, end_x, end_y = box.astype("int")
+        text = "{:.2f}%".format(confidence * 100)
+        y = start_y - 10 if start_y - 10 > 10 else start_y + 10
+        cv2.rectangle(image, (start_x, start_y), (end_x, end_y), (0, 0, 255), 2)
+        cv2.putText(
+            image, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2
+        )
+    cv2.imwrite(os.path.join(output_directory, os.path.basename(file)), image)
