34c12d713d1e94a80ad60c89ba0977de60fa599c
zenjiro <zenjiro0123@gmail.com>
2021-01-05 23:10:40 +0900

    変数名を変更、代入の左辺を明示的にタプルにしない。

--- detect_face_sample.py
+++ detect_face_sample.py
@@ -12,7 +12,7 @@ net = cv2.dnn.readNetFromCaffe(prototxt, model)
 # 解析対象の画像を読み込み
 image = cv2.imread("face.jpg")
 # 300x300に画像をリサイズ、画素値を調整
-(h, w) = image.shape[:2]
+h, w = image.shape[:2]
 blob = cv2.dnn.blobFromImage(
     cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0)
 )
@@ -28,11 +28,11 @@ for i in range(0, detections.shape[2]):
         continue
     # 検出結果を描画
     box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
+    start_x, start_y, end_x, end_y = box.astype("int")
     text = "{:.2f}%".format(confidence * 100)
-    y = startY - 10 if startY - 10 > 10 else startY + 10
-    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    y = start_y - 10 if start_y - 10 > 10 else start_y + 10
+    cv2.rectangle(image, (start_x, start_y), (end_x, end_y), (0, 0, 255), 2)
     cv2.putText(
-        image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2
+        image, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2
     )
 cv2.imwrite("result.jpg", image)
