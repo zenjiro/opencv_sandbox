a9da54819fa281539bf493a4b52616180039c012
zenjiro <zenjiro0123@gmail.com>
2021-01-07 22:13:19 +0900

    変数名を変更

--- detect_face.py
+++ detect_face.py
@@ -15,7 +15,7 @@ output_directory = "output"
 os.makedirs(output_directory, exist_ok=True)
 for file in sum([glob.glob(x) for x in sys.argv[1:]], []):
     image = cv2.imread(file)
-    h, w = image.shape[:2]
+    height, width = image.shape[:2]
     blob = cv2.dnn.blobFromImage(
         cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0)
     )
@@ -25,12 +25,12 @@ for file in sum([glob.glob(x) for x in sys.argv[1:]], []):
         confidence = detections[0, 0, i, 2]
         if confidence < confidence_limit:
             continue
-        box = detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-        start_x, start_y, end_x, end_y = box.astype("int")
+        box = detections[0, 0, i, 3:7] * numpy.array([width, height, width, height])
+        x1, y1, x2, y2 = box.astype("int")
         text = "{:.0f}%".format(confidence * 100)
-        y = start_y - 10 if start_y - 10 > 10 else start_y + 10
+        y = y1 - 10 if y1 - 10 > 10 else y1 + 10
         # Scarlet Red #ef2929 or Chameleon #8ae234
         color = (41, 41, 239) if confidence < confidence_limit2 else (52, 226, 138)
-        cv2.rectangle(image, (start_x, start_y), (end_x, end_y), color, 20)
-        cv2.putText(image, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX, 4, color, 20)
+        cv2.rectangle(image, (x1, y1), (x2, y2), color, 20)
+        cv2.putText(image, text, (x1, y), cv2.FONT_HERSHEY_SIMPLEX, 4, color, 20)
     cv2.imwrite(os.path.join(output_directory, os.path.basename(file)), image)
